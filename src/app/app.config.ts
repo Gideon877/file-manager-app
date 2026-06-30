e18523e93d1694d3d138b2168d696c8b2f3bad12
Thabang Gideon Magaola <gideon877@live.com>
2026-06-30 17:34:34 +0200

    ui - dependency clash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 import { APOLLO_OPTIONS, Apollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { routes } from './app.routes';
 
 const APOLLO_URI = 'http://localhost:4006/graphql';
 
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+export function createApollo(httpLink: HttpLink): ApolloClient.Options {
     return {
         link: httpLink.create({ uri: APOLLO_URI }),
+
         cache: new InMemoryCache(),
         defaultOptions: {
             watchQuery: {
                 fetchPolicy: 'network-only',
-                errorPolicy: 'all',
             },
         },
     };
